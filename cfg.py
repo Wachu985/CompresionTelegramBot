b23fab46cb9464cf506c4916b62653a0eef86dbc
Wachu985 <93799115+Wachu985@users.noreply.github.com>
2022-08-25 03:47:40 -0400

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,7 +1,7 @@
 """Configuraciones del BOT"""
-API_HASH = '213e85670cd03dfdcfc4936c86d153a2'
-API_ID = 15091118
-BOT_TOKEN = '5336546424:AAFFhZ0TCR1dWrCb-S4o9PL6NeZ5qMaTX5Q'
-BOT_NAME = 'pinochobot'
+API_HASH = ''
+API_ID = 
+BOT_TOKEN = ''
+BOT_NAME = ''
 BOT_URL = f'https://{BOT_NAME}.herokuapp.com'
 TIME_WAKE = 10
