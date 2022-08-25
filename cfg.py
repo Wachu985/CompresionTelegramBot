e90e8497d566d8d2e9502535c8ef4804b4862c84
Wachu985 <pedrobonilla985@gmal.com>
2022-08-24 20:15:01 -0400

    Update Server

--- cfg.py
+++ cfg.py
@@ -2,6 +2,6 @@
 API_HASH = '213e85670cd03dfdcfc4936c86d153a2'
 API_ID = 15091118
 BOT_TOKEN = '5336546424:AAFFhZ0TCR1dWrCb-S4o9PL6NeZ5qMaTX5Q'
-BOT_NAME = 'compresswachutelegram'
+BOT_NAME = 'pinochod83@gmail.com'
 BOT_URL = f'https://{BOT_NAME}.herokuapp.com'
 TIME_WAKE = 10
