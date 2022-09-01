90ff62263085584d4ff8eedb9d3d256ee2470183
Wachu985 <pedrobonilla985@gmal.com>
2022-09-01 05:55:22 -0400

    Update YTDL

--- downloader/youtubedl.py
+++ downloader/youtubedl.py
@@ -48,7 +48,7 @@ class YoutubeDL():
             if d['speed'] is not None:
                 speed = d['speed']
             tiempo = d['_eta_str']
-            self.downlad_progres(int(current), int(total),speed,pingo,tiempo,self.msg,self.bot)
+            self.downlad_progres(int(current), int(total),speed,filename,tiempo,self.msg,self.bot)
         if d['status'] == 'finished':
             print('Done downloading, now converting ...')
 
